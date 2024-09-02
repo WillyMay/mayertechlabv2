3c760833a326648d0d8ba7636e3ddc71e1a1167e
WillyMay <willian.mayer@proton.me>
2024-09-02 10:17:04 -0400

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar"
 
 function App() {
  
   return (
     <>
+      <Navbar />
       <h1 className='text-3xl underline'>Hola! Estoy actualizando mi pagina</h1>
     </>
   )
