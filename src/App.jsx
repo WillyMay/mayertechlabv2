f5960927871021dd8179a64c256faeab235b9318
WillyMay <willian.mayer@proton.me>
2024-11-04 17:34:46 -0300

    adding new routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/NavbarMenu";
+import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Blog from "./components/Blog";
 import NoPage from "./components/NoPage";
-import Services from "./components/Services";
+import Servicios from "./components/Servicios";
+import Nosotros from "./components/Nosotros";
+import Contacto from "./components/Contacto";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navbar />}>
           <Route index element={<Home />} />
-          <Route path="blogs" element={<Blog />} />
-          <Route path="services" element={<Services />} />
+          <Route path="blog" element={<Blog />} />
+          <Route path="servicios" element={<Servicios />} />
+          <Route path="nosotros" element={<Nosotros />} />
+          <Route path="contacto" element={<Contacto />} />
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Services.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Services() {
-  return (
-    <div>Hello from services!</div>
-  )
-}
-
-export default Services
