05797ab2a14adfbff5b6cb9bf378ec7c19df641c
WillyMay <willian.mayer@proton.me>
2024-11-04 10:02:01 -0300

    installing react-router and components

---
+++ src/components/Blog.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Blog() {
+  return (
+    <div>Hello from blog!</div>
+  )
+}
+
+export default Blog
---
+++ src/components/Services.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Services() {
+  return (
+    <div>Hello from services!</div>
+  )
+}
+
+export default Services
